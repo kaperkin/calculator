1a12be82476add251681362c9eab6314412c7d76
kaperkin <kaleena.perkins@gmail.com>
2016-01-24 11:39:52 -0800

    functionality in jQuery and JS

--- js/app.js
+++ js/app.js
@@ -1,97 +1,171 @@
-(function () {
-    'use strict';
+//(function () {
+//    'use strict';
+//
+//    var numbers = '';
+//    var equation = [];
+//    var buttons = document.getElementsByTagName('button');
+//    for (var b = 0; b < buttons.length; b++) {
+//        buttons[b].addEventListener('click', function () {
+//            var val = this.value;
+//            if (val === '0') {
+//                add('0');
+//            }
+//            if (Number(val) || val == '.') {
+//                add(this.value);
+//            }
+//            switch (val) {
+//                case '=':
+//                    equals(equation);
+//                    break;
+//                case '-':
+//                    pushIt('-');
+//                    break;
+//                case '+':
+//                    pushIt('+');
+//                    break;
+//                case '*':
+//                    pushIt('*');
+//                    break;
+//                case '/':
+//                    pushIt('/');
+//                    break;
+//                case 'AC':
+//                    numbers = '';
+//                    equation = [];
+//                    total.innerHTML = '';
+//                    break;
+//                case 'CE':
+//                    if (numbers.length > 0) {
+//                        numbers = '';
+//                        total.innerHTML = equation[equation.length - 2];
+//                    } else {
+//                        equation.pop();
+//                        if (numbers.length == 0 && equation.length == 0) {
+//                            total.innerHTML = 0;
+//                        } else {
+//                            total.innerHTML = equation[equation.length - 1];
+//                        }
+//                    }
+//                    break;
+//            }
+//
+//        })
+//    }
+//
+//    function pushIt(val) {
+//        equation.push(numbers, val);
+//        numbers = '';
+//    }
+//
+//    function add(num) {
+//        console.log(num);
+//        console.log(typeof num);
+//        numbers += num;
+//        total.innerHTML = numbers;
+//    }
+//
+//    function equals(equation) {
+//        equation.push(numbers);
+//        var answer = Number(equation[0]);
+//
+//        for (var i = 1; i < equation.length; i++) {
+//            var next = equation[i + 1];
+//            if (equation[i] == '+') {
+//                answer += Number(next);
+//            } else if (equation[i] == '-') {
+//                answer -= Number(next);
+//            } else if (equation[i] == '*') {
+//                answer *= Number(next);
+//            } else if (equation[i] == '/') {
+//                answer /= Number(next);
+//            }
+//        }
+//        total.innerHTML = answer;
+//        numbers = '';
+//    }
+//})
+//();
 
-    var numbers = '';
-    var equation = [];
-    var buttons = document.getElementsByTagName('button');
-    for (var b = 0; b < buttons.length; b++) {
-        buttons[b].addEventListener('click', function () {
+( function ($) {
+    'use strict';
+    $(document).ready(function () {
+        var numbers = '';
+        var equation = [];
+
+        $("button").on('click', function () {
             var val = this.value;
+ if (val === '0') {
+                add('0');
+            }
             if (Number(val) || val == '.') {
                 add(this.value);
-            } else if (val == '=') {
-                equals(equation)
-            } else if (val == '-') {
-                pushIt('-')
-            } else if (val == '+') {
-                pushIt('+')
-            } else if (val == '*') {
-                pushIt('*')
-            } else if (val == '/') {
-                pushIt('/')
-            } else if (val == 'AC') {
-                numbers = '';
-                equation = [];
-                total.innerHTML = '';
-            } else if (val == 'CE') {
-                if (numbers.length > 0) {
+            }
+            switch (val) {
+                case '=':
+                    equals(equation);
+                    break;
+                case '-':
+                    pushIt('-');
+                    break;
+                case '+':
+                    pushIt('+');
+                    break;
+                case '*':
+                    pushIt('*');
+                    break;
+                case '/':
+                    pushIt('/');
+                    break;
+                case 'AC':
                     numbers = '';
-                    total.innerHTML = equation[equation.length - 2];
-                } else {
-                    equation.pop();
-                    if (numbers.length == 0 && equation.length == 0) {
-                        total.innerHTML = 0;
+                    equation = [];
+                    total.innerHTML = '';
+                    break;
+                case 'CE':
+                    if (numbers.length > 0) {
+                        numbers = '';
+                        total.innerHTML = equation[equation.length - 2];
                     } else {
-                        total.innerHTML = equation[equation.length - 1];
+                        equation.pop();
+                        if (numbers.length == 0 && equation.length == 0) {
+                            total.innerHTML = 0;
+                        } else {
+                            total.innerHTML = equation[equation.length - 1];
+                        }
+                    }
+                    break;
+            }
+            function pushIt(val) {
+                equation.push(numbers, val);
+                numbers = '';
+            }
+
+            function add(num) {
+                numbers += num;
+                total.innerHTML = numbers;
+                console.log(equation, numbers);
+            }
+
+            function equals(equation) {
+                equation.push(numbers);
+                var answer = Number(equation[0]);
+
+                for (var i = 1; i < equation.length; i++) {
+                    var next = equation[i + 1];
+                    if (equation[i] == '+') {
+                        answer += Number(next);
+                    } else if (equation[i] == '-') {
+                        answer -= Number(next);
+                    } else if (equation[i] == '*') {
+                        answer *= Number(next);
+                    } else if (equation[i] == '/') {
+                        answer /= Number(next);
                     }
                 }
+                total.innerHTML = answer;
+                numbers = '';
             }
-        })
-    }
+        });
 
-    function pushIt(val) {
-        equation.push(numbers, val);
-        numbers = '';
-    }
-    
-    function add(num) {
-        numbers += num;
-        total.innerHTML = numbers;
-        console.log(equation, numbers);
-    }
-
-    function equals(equation) {
-        equation.push(numbers);
-        var answer = Number(equation[0]);
-
-        for (var i = 1; i < equation.length; i++) {
-            var next = equation[i + 1];
-            if (equation[i] == '+') {
-                answer += Number(next);
-            } else if (equation[i] == '-') {
-                answer -= Number(next);
-            } else if (equation[i] == '*') {
-                answer *= Number(next);
-            } else if (equation[i] == '/') {
-                answer /= Number(next);
-            }
-        }
-        total.innerHTML = answer;
-        numbers = '';
-        equation = [];
-    }
-
-})();
-
-//( function ($) {
-//    'use strict';
-//    $(document).ready(function () {
-//        var temp = '';
-//        var entries =[];
-//        var total = 0;
-//        $("button").on('click', function () {
-//            var val = this.value;
-//            if (Number(val) || val == '.') {
-//                temp += val;
-//                $('#total').text(temp);
-//            } else if(val == 'AC'){
-//                temp = '';
-//                entries = [];
-//                total = 0;
-//                $('#total').text(temp);
-//            } else if (val == 'CE'){
-//                console.log('NAN');
-//            }
-//        });
-//    })
-//}(jQuery) );
+    })
+}(jQuery) );
